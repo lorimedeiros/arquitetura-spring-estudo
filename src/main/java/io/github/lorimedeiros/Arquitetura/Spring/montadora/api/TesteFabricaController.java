c606e5905f4f644007c938d6dcbe8e12782029a7
Lori Medeiros <cecilialmedeirosc@gmail.com>
2025-04-04 22:22:50 -0300

    adicionando endpoint para testar fluxo no postman/insomnia

--- src/main/java/io/github/lorimedeiros/Arquitetura/Spring/montadora/api/TesteFabricaController.java
+++ src/main/java/io/github/lorimedeiros/Arquitetura/Spring/montadora/api/TesteFabricaController.java
@@ -4,9 +4,11 @@ import io.github.lorimedeiros.Arquitetura.Spring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired //"Spring vá lá no seu container e me dê uma instancia de motor", no caso, aquela instancia que temos lá em MontadoraConfig
